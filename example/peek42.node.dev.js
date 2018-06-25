6b1755e730af64db84ab2e880c57131b3d1cf09a
Radoslav Peev <rpeev@ymail.com>
2018-06-25 08:19:36 +0300

    Move examples to dedicated folder

--- example/peek42.node.dev.js
+++ example/peek42.node.dev.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const apivis = require('apivis');
-const peek42 = require('./dist/peek42.cjs');
+const peek42 = require('../dist/peek42.cjs');
 const {p, pp} = peek42.use(apivis);
 
 p.api(peek42);
