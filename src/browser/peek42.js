8943bf74bf3386df28c973bc9bd92ac3734e9638
Radoslav Peev <rpeev@ymail.com>
2018-07-03 12:14:53 +0300

    Hook and report window error and unhandled rejection

--- src/browser/peek42.js
+++ src/browser/peek42.js
@@ -18,4 +18,38 @@ Object.assign(peek42, {
   _output
 });
 
+function _reportError(err, note = 'error') {
+  const {pp} = peek42;
+
+  if (window.bundleError &&
+    err.sourceURL && err.sourceURL.endsWith('bundle.js')
+  ) {
+    pp(err,
+      `(${note}) ${err} (retrieving source information...)`,
+      {level: 'error'});
+
+    bundleError.addSrcInfo(err).then(err1 => {
+      pp(err1.srcSnip,
+        `(${note}) ${err1}`,
+        {level: 'error'});
+    }).catch(err2 => {
+      pp(err2,
+        `(${note}) ${err2}`,
+        {level: 'error'});
+    });
+  } else {
+    pp(err,
+      `(${note}) ${err}`,
+      {level: 'error'});
+  }
+}
+
+window.addEventListener('error', ev => {
+  _reportError(ev.error);
+});
+
+window.addEventListener('unhandledrejection', ev => {
+  _reportError(ev.reason, 'unhandledrejection');
+});
+
 export default peek42;
