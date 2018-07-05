c664601fba9e3fa4171a5fb54f1b967877f73628
Radoslav Peev <rpeev@ymail.com>
2018-07-05 08:56:12 +0300

    Add stack info to error reporting

--- src/browser/error.js
+++ src/browser/error.js
@@ -1,26 +1,29 @@
-import {pp} from '../universal/base';
+import {pretty, p, pp} from '../universal/base';
+
+function _info(err) {
+  return `${pretty(err)}\n  ${
+    (err &&
+      err.stack &&
+      err.stack.replace(/\n/gm, '\n  ')
+    ) || 'no stack'
+  }`;
+}
 
 function _reportError(err, note = 'error') {
   if (window.bundleError &&
     err.sourceURL && err.sourceURL.endsWith('bundle.js')
   ) {
-    pp(err,
+    p(`${_info(err)}`,
       `(${note}) ${err} (retrieving source information...)`,
       {level: 'error'});
 
     bundleError.addSrcInfo(err).then(err1 => {
-      pp(err1.srcSnip,
-        `(${note}) ${err1}`,
-        {level: 'error'});
+      pp(err1.srcSnip, `(${note}) ${err1}`, {level: 'error'});
     }).catch(err2 => {
-      pp(err2,
-        `(${note}) ${err2}`,
-        {level: 'error'});
+      p(`${_info(err2)}`, `(${note}) ${err2}`, {level: 'error'});
     });
   } else {
-    pp(err,
-      `(${note}) ${err}`,
-      {level: 'error'});
+    p(`${_info(err)}`, `(${note}) ${err}`, {level: 'error'});
   }
 }
 
