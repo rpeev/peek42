47542d3e160c2845b9d5f55c32c9ae3d9c77a4bb
Radoslav Peev <rpeev@ymail.com>
2018-07-17 12:34:10 +0300

    Tweak examples

--- example/peek42.node.js
+++ example/peek42.node.js
@@ -2,8 +2,8 @@
 'use strict';
 
 const apivis = require('apivis');
-const peek42 = require('../dist/peek42.node');
-//const peek42 = require('peek42');
-  const {p, pp} = peek42.use(apivis);
+const peek42 = require('../dist/peek42.node').use(apivis);
+//const peek42 = require('peek42').use(apivis);
+  const {p, pp} = peek42;
 
 p.api(process);
