9a6cdc5050fb95d8602c9ec3b414072cef0e8261
Radoslav Peev <rpeev@ymail.com>
2018-07-17 15:53:45 +0300

    Tweak error reporting code

--- src/browser/error.js
+++ src/browser/error.js
@@ -1,6 +1,8 @@
 import {pretty, p, pp} from '../universal/base';
 
-function _info(err) {
+let _errorId = 1;
+
+function _formatError(err) {
   return `${pretty(err)}\n  ${
     (err &&
       err.stack &&
@@ -13,17 +15,25 @@ function _reportError(err, note = 'error') {
   if (window.bundleError &&
     err.sourceURL && err.sourceURL.endsWith('bundle.js')
   ) {
-    p(`${_info(err)}`,
-      `(${note}) ${err} (retrieving source information...)`,
+    err._errorId = _errorId++;
+
+    p(`${_formatError(err)}`,
+      `(${note} (id: ${err._errorId})) ${err} (retrieving source information...)`,
       {level: 'error'});
 
     bundleError.addSrcInfo(err).then(err1 => {
-      pp(err1.srcSnip, `(${note}) ${err1}`, {level: 'error'});
+      pp(err1.srcSnip,
+        `(${note} (id: ${err._errorId})) ${err1}`,
+        {level: 'error'});
     }).catch(err2 => {
-      p(`${_info(err2)}`, `(${note}) ${err2}`, {level: 'error'});
+      p(`${_formatError(err2)}`,
+        `(${note} (id: ${err._errorId})) ${err2}`,
+        {level: 'error'});
     });
   } else {
-    p(`${_info(err)}`, `(${note}) ${err}`, {level: 'error'});
+    p(`${_formatError(err)}`,
+      `(${note}) ${err}`,
+      {level: 'error'});
   }
 }
 
