d607b3f7f49232f0aa0893eb54beaff535ced8a1
Radoslav Peev <rpeev@ymail.com>
2018-08-20 15:33:45 +0300

    Show error source traces for transpiled babel inline scripts

--- src/browser/error.js
+++ src/browser/error.js
@@ -2,7 +2,7 @@ import {pretty, p, pp} from '../universal/base';
 
 let _errorId = 1;
 
-async function _getSourceAsync(err) {
+async function errorSourceAsync(err) {
   return new Promise(async (resolve, reject) => {
     if (!err.sourceURL) {
       return reject(Error('sourceURL unavailable'));
@@ -24,7 +24,123 @@ async function _getSourceAsync(err) {
   });
 }
 
-function _genSourceTrace(source, location, line1, column1, {
+function isInlineScriptError(err) {
+  return err.sourceURL && err.sourceURL === window.location.href;
+}
+
+function isTranspiledScriptError(err) {
+  return !!(
+    err.stack &&
+    err.stack.match(/\nrun([^\n])+babel/m)
+  );
+}
+
+function inlineScripts() {
+  return Array.from(document.scripts).
+    filter(script => !script.getAttribute('src')).
+    map(script => ({
+      source: script.textContent,
+      sourceMap: undefined,
+      originalErrorInfo: undefined
+    }));
+}
+
+function transpiledInlineScripts(_inlineScripts) {
+  return _inlineScripts.
+    map(scriptInfo => {
+      scriptInfo.sourceMap = embeddedSourceMap(
+        sourceMappingURL(scriptInfo.source) || ''
+      );
+
+      return scriptInfo;
+    }).
+    filter(scriptInfo => !!scriptInfo.sourceMap);
+}
+
+async function transpiledInlineScriptsWithOriginalErrorInfoAsync(
+  _transpiledInlineScripts,
+  err
+) {
+  return Promise.all(
+    _transpiledInlineScripts.
+      map(async scriptInfo => {
+        scriptInfo.originalErrorInfo = await originalErrorInfoAsync(
+          scriptInfo.sourceMap, err.line, err.column
+        );
+
+        return scriptInfo;
+      })
+  );
+}
+
+async function transpiledInlineScriptsSourceTracesAsync(err) {
+  let scripts = transpiledInlineScripts(inlineScripts());
+
+  try {
+    return (await transpiledInlineScriptsWithOriginalErrorInfoAsync(
+      scripts,
+      err
+    )).
+      //filter(scriptInfo => !!scriptInfo.originalErrorInfo.position.name).
+      map(scriptInfo => sourceTrace(
+        ...sourceTraceArgs(scriptInfo.originalErrorInfo)
+      ));
+  } catch (err1) {
+    return scripts.
+      map((scriptInfo, i) => sourceTrace(
+        scriptInfo.source, `Transpiled script #${i + 1}`, err.line, err.column
+      ));
+  }
+}
+
+function sourceMappingURL(source) {
+  let reSourceMappingURL =
+    /\/\/# sourceMappingURL=(.+)\s*$/m;
+
+  return (source.match(reSourceMappingURL) || [])[1];
+}
+
+function embeddedSourceMap(srcMappingURL) {
+  let reEmbeddedSourceMap =
+    /^data:application\/json;(charset=[^;]+;)?base64,(.*)$/;
+  let base64 = (srcMappingURL.match(reEmbeddedSourceMap) || [])[2];
+
+  return (base64) ? JSON.parse(atob(base64)) : base64;
+}
+
+function originalErrorInfoAsync(srcMap, runtimeErrLine1, runtimeErrColumn1) {
+  const {SourceMapConsumer} = sourceMap;
+
+  return SourceMapConsumer.with(srcMap, null, consumer => {
+    let position = consumer.originalPositionFor({
+      line: runtimeErrLine1,
+      // The sourceMap lib expects 0-based columns
+      column: runtimeErrColumn1 - 1
+    });
+    let source = consumer.sourceContentFor(position.source);
+
+    return {
+      position,
+      source
+    };
+  });
+}
+
+function sourceTraceArgs(originalErrorInfo) {
+  let {source, position} = originalErrorInfo;
+
+  return [
+    source,
+    position.source,
+    position.line,
+    // The sourceTrace func expects 1-based columns
+    // position.name is the erroneous token
+    // Place the error marker after it for consistency with runtime errors
+    position.column + 1 + (position.name || '').length
+  ];
+}
+
+function sourceTrace(source, location, line1, column1, {
   indent = '',
   peekLines = 2
 } = {}) {
@@ -61,12 +177,11 @@ async function _formatError(err) {
   let info;
 
   try {
-    info = _genSourceTrace(
-      await _getSourceAsync(err),
-      err.sourceURL,
-      err.line,
-      err.column
-    );
+    info = (isInlineScriptError(err) && isTranspiledScriptError(err)) ?
+      (await transpiledInlineScriptsSourceTracesAsync(err)).
+        join('\n') :
+      sourceTrace(await errorSourceAsync(err),
+        err.sourceURL, err.line, err.column);
   } catch (err1) {
     err.sourceTraceNA = `${err1}`;
 
