650d65c2bc3014bae6732e13e1965a2751ccd3c8
Radoslav Peev <rpeev@ymail.com>
2018-08-21 12:39:41 +0300

    Sync error related functions with private codebase

--- src/browser/error.js
+++ src/browser/error.js
@@ -1,6 +1,78 @@
 import {pretty, p, pp} from '../universal/base';
 
-let _errorId = 1;
+function isInlineScriptError(err) {
+  return !!(
+    err.sourceURL &&
+    err.sourceURL === window.location.href
+  );
+}
+
+function isTranspiledScriptError(err) {
+  return !!(
+    err.stack &&
+    err.stack.match(/\nrun([^\n])+babel/m)
+  );
+}
+
+function inlineScripts() {
+  return Array.from(document.scripts).
+    filter(script => !script.getAttribute('src')).
+    map(script => ({
+      source: script.textContent,
+      sourceMap: null,
+      originalErrorInfo: null
+    }));
+}
+
+function transpiledInlineScripts(_inlineScripts) {
+  return _inlineScripts.
+    map(scriptInfo => {
+      scriptInfo.sourceMap = embeddedSourceMap(
+        sourceMappingURL(scriptInfo.source)
+      );
+
+      return scriptInfo;
+    }).
+    filter(scriptInfo => !!scriptInfo.sourceMap);
+}
+
+async function transpiledInlineScriptsWithOriginalErrorInfoAsync(
+  _transpiledInlineScripts,
+  err
+) {
+  return Promise.all(
+    _transpiledInlineScripts.
+      map(async scriptInfo => {
+        scriptInfo.originalErrorInfo = await originalErrorInfoAsync(
+          scriptInfo.sourceMap, err
+        );
+
+        return scriptInfo;
+      })
+  );
+}
+
+async function transpiledInlineScriptsSourceTracesAsync(err) {
+  let scripts = transpiledInlineScripts(inlineScripts());
+
+  try {
+    return (await transpiledInlineScriptsWithOriginalErrorInfoAsync(
+      scripts,
+      err
+    )).
+      map(scriptInfo => sourceTrace(
+        ...sourceTraceArgs(scriptInfo.originalErrorInfo)
+      ));
+  } catch (err1) {
+    return scripts.
+      map((scriptInfo, i) => sourceTrace(
+        scriptInfo.source,
+        `Transpiled inline script #${i + 1}`,
+        err.line,
+        err.column
+      ));
+  }
+}
 
 async function errorSourceAsync(err) {
   return new Promise(async (resolve, reject) => {
@@ -24,75 +96,6 @@ async function errorSourceAsync(err) {
   });
 }
 
-function isInlineScriptError(err) {
-  return err.sourceURL && err.sourceURL === window.location.href;
-}
-
-function isTranspiledScriptError(err) {
-  return !!(
-    err.stack &&
-    err.stack.match(/\nrun([^\n])+babel/m)
-  );
-}
-
-function inlineScripts() {
-  return Array.from(document.scripts).
-    filter(script => !script.getAttribute('src')).
-    map(script => ({
-      source: script.textContent,
-      sourceMap: undefined,
-      originalErrorInfo: undefined
-    }));
-}
-
-function transpiledInlineScripts(_inlineScripts) {
-  return _inlineScripts.
-    map(scriptInfo => {
-      scriptInfo.sourceMap = embeddedSourceMap(
-        sourceMappingURL(scriptInfo.source) || ''
-      );
-
-      return scriptInfo;
-    }).
-    filter(scriptInfo => !!scriptInfo.sourceMap);
-}
-
-async function transpiledInlineScriptsWithOriginalErrorInfoAsync(
-  _transpiledInlineScripts,
-  err
-) {
-  return Promise.all(
-    _transpiledInlineScripts.
-      map(async scriptInfo => {
-        scriptInfo.originalErrorInfo = await originalErrorInfoAsync(
-          scriptInfo.sourceMap, err.line, err.column
-        );
-
-        return scriptInfo;
-      })
-  );
-}
-
-async function transpiledInlineScriptsSourceTracesAsync(err) {
-  let scripts = transpiledInlineScripts(inlineScripts());
-
-  try {
-    return (await transpiledInlineScriptsWithOriginalErrorInfoAsync(
-      scripts,
-      err
-    )).
-      //filter(scriptInfo => !!scriptInfo.originalErrorInfo.position.name).
-      map(scriptInfo => sourceTrace(
-        ...sourceTraceArgs(scriptInfo.originalErrorInfo)
-      ));
-  } catch (err1) {
-    return scripts.
-      map((scriptInfo, i) => sourceTrace(
-        scriptInfo.source, `Transpiled script #${i + 1}`, err.line, err.column
-      ));
-  }
-}
-
 function sourceMappingURL(source) {
   let reSourceMappingURL =
     /\/\/# sourceMappingURL=(.+)\s*$/m;
@@ -100,24 +103,24 @@ function sourceMappingURL(source) {
   return (source.match(reSourceMappingURL) || [])[1];
 }
 
-function embeddedSourceMap(srcMappingURL) {
+function embeddedSourceMap(_sourceMappingURL) {
   let reEmbeddedSourceMap =
     /^data:application\/json;(charset=[^;]+;)?base64,(.*)$/;
-  let base64 = (srcMappingURL.match(reEmbeddedSourceMap) || [])[2];
+  let base64 = ((_sourceMappingURL || '').match(reEmbeddedSourceMap) || [])[2];
 
-  return (base64) ? JSON.parse(atob(base64)) : base64;
+  return (base64) ? JSON.parse(atob(base64)) : null;
 }
 
-function originalErrorInfoAsync(srcMap, runtimeErrLine1, runtimeErrColumn1) {
+function originalErrorInfoAsync(_sourceMap, err) {
   const {SourceMapConsumer} = sourceMap;
 
-  return SourceMapConsumer.with(srcMap, null, consumer => {
+  return SourceMapConsumer.with(_sourceMap, null, consumer => {
     let position = consumer.originalPositionFor({
-      line: runtimeErrLine1,
+      line: err.line || 1,
       // The sourceMap lib expects 0-based columns
-      column: runtimeErrColumn1 - 1
-    });
-    let source = consumer.sourceContentFor(position.source);
+      column: (err.column || 1) - 1
+    }) || {};
+    let source = consumer.sourceContentFor(position.source, true) || '';
 
     return {
       position,
@@ -126,17 +129,17 @@ function originalErrorInfoAsync(srcMap, runtimeErrLine1, runtimeErrColumn1) {
   });
 }
 
-function sourceTraceArgs(originalErrorInfo) {
-  let {source, position} = originalErrorInfo;
+function sourceTraceArgs(_originalErrorInfo) {
+  let {source, position} = _originalErrorInfo;
 
   return [
     source,
-    position.source,
-    position.line,
+    position.source || '',
+    position.line || 1,
     // The sourceTrace func expects 1-based columns
     // position.name is the erroneous token
     // Place the error marker after it for consistency with runtime errors
-    position.column + 1 + (position.name || '').length
+    (position.column || 0) + 1 + (position.name || '').length
   ];
 }
 
@@ -198,32 +201,12 @@ async function _formatErrorAsync(err) {
   }`;
 }
 
-async function _reportError(err, note = 'error') {
-  if (window.bundleError &&
-    err.sourceURL && err.sourceURL.endsWith('bundle.js')
-  ) {
-    err._errorId = _errorId++;
-
-    p(`${await _formatErrorAsync(err)}`,
-      `(${note} (id: ${err._errorId})) ${err} (retrieving source information...)`,
-      {level: 'error'});
-
-    try {
-      await bundleError.addSrcInfo(err);
-
-      pp(err.srcSnip,
-        `(${note} (id: ${err._errorId})) ${err}`,
-        {level: 'error'});
-    } catch(err1) {
-      p(`${await _formatErrorAsync(err1)}`,
-        `(${note} (id: ${err._errorId})) ${err1}`,
-        {level: 'error'});
-    }
-  } else {
-    p(`${await _formatErrorAsync(err)}`,
+function _reportError(err, note = 'error') {
+  _formatErrorAsync(err).then(str => {
+    p(str,
       `(${note}) ${err}`,
       {level: 'error'});
-  }
+  });
 }
 
 export {
