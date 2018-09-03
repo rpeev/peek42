95da808665d036ab2fab177164f4d7f9dfd1e0ba
Radoslav Peev <rpeev@ymail.com>
2018-09-03 15:26:33 +0300

    Add the ability to expand/collapse log entries

--- src/browser/console/log-entry.js
+++ src/browser/console/log-entry.js
@@ -1,3 +1,17 @@
+function onLogEntryToggleClick(ev) {
+  let elComment = ev.currentTarget;
+  let elToggle = elComment.firstElementChild;
+  let elMessage = elComment.nextElementSibling;
+
+  if (elMessage.style.display === 'none') {
+    elToggle.innerHTML = '&#x25be;';
+    elMessage.style.display = '';
+  } else {
+    elToggle.innerHTML = '&#x25b8;';
+    elMessage.style.display = 'none';
+  }
+}
+
 function addLogEntry({
   elLog,
   textContent,
@@ -15,8 +29,10 @@ function addLogEntry({
   } else {
     elEntry.classList.add('peek42-log-entry-complex');
     elEntry.innerHTML = `<div class="peek42-log-entry-comment">\
-<span class="peek42-log-entry-toggle">&#x25bd;</span>${comment}</div>\
+<span class="peek42-log-entry-toggle">&#x25be;</span>${comment}</div>\
 <div class="peek42-log-entry-message">${message}</div>`;
+    elEntry.firstElementChild.
+      addEventListener('click', onLogEntryToggleClick);
   }
 
   elLog.insertBefore(elEntry, elLog.firstChild);
